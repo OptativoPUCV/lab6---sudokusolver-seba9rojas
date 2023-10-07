0afa1055861b5550dab48b78a66983f655864c06
Replit user <>
2023-10-07 01:40:37 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -82,10 +82,10 @@ int is_final(Node* n){
     for(int j=0;j<9;j++){
       if(n->sudo[i][j]==0){
         return 1;
-      
+      }
     }
   }
-  return 0;
+  return 1;
 }
 
 Node* DFS(Node* initial, int* cont){
